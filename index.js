4a19ec66a6a6556a65aac7065fe3ef06606190ae
aldinkapetanovic@gmail.com <aldinkapetanovic@gmail.com>
2023-05-19 17:38:07 +0200

    viewport

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ const port = process.env.PORT || 3000
 
 app.use(morgan('combined'))
 
-app.get('/', (req, res) => res.send(`<div>Hello, I am <h1>${hostname}!</h1></div>`))
+app.get('/', (req, res) => res.send(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${hostname}</title>
+</head>
+
+<body>
+    <div>Hello, I am <h1>${hostname}!</h1>
+    </div>
+</body>
+
+</html>`))
 
 app.listen(port, (err) => {
   if (err) {
